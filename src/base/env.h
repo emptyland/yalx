6b02803a0a846bda7f979246b85514522e1d0708
emptyland <cjvoid@gmail.com>
2021-05-09 22:16:17 +0800

    Add os page allocation.

--- src/base/env.h
+++ src/base/env.h
@@ -3,6 +3,12 @@
 #define YALX_BASE_ENV_H_
 
 #include "base/status.h"
+#include "base/base.h"
+
+#if defined(YALX_OS_DARWIN)
+#include <sys/mman.h>
+#endif // defined(YALX_OS_DARWIN)
+
 #include <memory>
 
 namespace yalx {
@@ -13,15 +19,48 @@ class SequentialFile;
 
 class Env {
 public:
+    static constexpr int kMemoryReadable = 0x1;
+    static constexpr int kMemoryWriteable = 0x2;
+    static constexpr int kMemoryExecuteable = 0x4;
+    
     // std::unique_ptr<SequentialFile> file;
     // auto rs = Env::NewSequentialFile("file", &file);
     // if (rs.fail()) {
     //     return rs;
     // }
     static Status NewSequentialFile(const std::string &name, std::unique_ptr<SequentialFile> *file);
+    
+    static inline Status OSPagedAllocate(size_t n, int access, void **result);
+    
+    static inline Status OSPagedFree(void *chunk, size_t n);
 };
 
 
+#if defined(YALX_OS_POSIX)
+
+inline Status Env::OSPagedAllocate(size_t n, int access, void **result) {
+    int flags = MAP_ANON|MAP_PRIVATE;
+#if defined(YALX_OS_DARWIN)
+    if (access & (kMemoryWriteable | kMemoryExecuteable)) {
+        flags |= MAP_JIT;
+    }
+#endif // defined(YALX_OS_DARWIN)
+    auto chunk = ::mmap(nullptr, n, access, MAP_ANON|MAP_PRIVATE, -1, 0);
+    if (chunk == MAP_FAILED) {
+        return ERR_CORRUPTION("mmap fail!");
+    }
+    
+    return Status::OK();
+}
+
+inline Status Env::OSPagedFree(void *chunk, size_t n) {
+    if (::munmap(chunk, n) < 0) {
+        return ERR_CORRUPTION("munmap fail!");
+    }
+    return Status::OK();
+}
+
+#endif // defined(YALX_OS_POSIX)
 
 } // namespace base
 
