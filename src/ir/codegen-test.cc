64ebb8864114f136dd58e2d4741fc8d61b6c4212
emptyland <cjvoid@gmail.com>
2022-01-21 21:31:24 +0800

    Add multi-line string for unit test(s).

--- src/ir/codegen-test.cc
+++ src/ir/codegen-test.cc
@@ -125,7 +125,119 @@ TEST_F(IntermediateRepresentationGeneratorTest, WhileLoop) {
     base::PrintingWriter printer(base::NewMemoryWritableFile(&buf), true/*ownership*/);
     modules["main:main"]->PrintTo(&printer);
     
-    printf("%s\n", buf.data());
+    const char *z = R"(module main @main:main {
+globals:
+interfaces:
+structures:
+functions:
+    fun $init(): void {
+    boot:
+        %0 = Closure ref[fun ()->void] <fun foo:foo.$init>
+        CallRuntime void ref[fun ()->void] %0, string "foo:foo" <PkgInitOnce>
+        %1 = Closure ref[fun ()->void] <fun yalx/lang:lang.$init>
+        CallRuntime void ref[fun ()->void] %1, string "yalx/lang:lang" <PkgInitOnce>
+        Ret void
+    } // main:main.$init
+
+    fun issue1(): void {
+    entry:
+        Br void out [L1:]
+    L1:
+        %0 = CallDirectly u8 <fun foo:foo.foo>
+        Br void u8 %0 out [L2:, L3:]
+    L2:
+        CallDirectly void string "loop" <fun yalx/lang:lang.println>
+        Br void out [L1:]
+    L3:
+        Ret void
+    } // main:main.issue1
+
+    fun issue2(): void {
+    entry:
+        Br void out [L2:]
+    L1:
+        CallDirectly void string "loop" <fun yalx/lang:lang.println>
+        Br void out [L2:]
+    L2:
+        %0 = CallDirectly u8 <fun foo:foo.bar>
+        Br void u8 %0 out [L1:, L3:]
+    L3:
+        Ret void
+    } // main:main.issue2
+
+    fun issue3(): void {
+    entry:
+        Br void out [L1:]
+    L1:
+        %0 = CallDirectly u8 <fun foo:foo.foo>
+        Br void u8 %0 out [L3:, L2:]
+    L2:
+        CallDirectly void string "loop" <fun yalx/lang:lang.println>
+        Br void out [L1:]
+    L3:
+        Ret void
+    } // main:main.issue3
+
+    fun issue4(): void {
+    entry:
+        Br void out [L2:]
+    L1:
+        CallDirectly void string "do {} unless" <fun yalx/lang:lang.println>
+        Br void out [L2:]
+    L2:
+        %0 = CallDirectly u8 <fun foo:foo.bar>
+        Br void u8 %0 out [L3:, L1:]
+    L3:
+        Ret void
+    } // main:main.issue4
+
+    fun issue5(): void {
+    entry:
+        Br void out [L1:]
+    L1:
+        %0 = Phi i32 i32 0, i32 %1 in [entry:, L5:]
+        %2 = ICmp u8 i32 %0, i32 10 <slt>
+        Br void u8 %2 out [L2:, L6:]
+    L2:
+        %3 = ICmp u8 i32 %0, i32 5 <slt>
+        Br void u8 %3 out [L3:, L4:]
+    L3:
+        %4 = Add i32 i32 %0, i32 1
+        Br void out [L5:]
+    L4:
+        %5 = Add i32 i32 %0, i32 2
+        Br void out [L5:]
+    L5:
+        %1 = Phi i32 i32 %0, i32 %4, i32 %5 in [entry:, L3:, L4:]
+        Br void out [L1:]
+    L6:
+        Ret void
+    } // main:main.issue5
+
+    fun issue6(): void {
+    entry:
+        Br void out [L2:]
+    L1:
+        %0 = Add i32 i32 %1, i32 1
+        Br void out [L2:]
+    L2:
+        %1 = Phi i32 i32 0, i32 %0 in [entry:, L1:]
+        %2 = ICmp u8 i32 %1, i32 20 <slt>
+        Br void u8 %2 out [L1:, L3:]
+    L3:
+        Ret void
+    } // main:main.issue6
+
+    fun main(): void {
+    entry:
+        Ret void
+    } // main:main.main
+
+} // @main:main
+)";
+    
+    ASSERT_EQ(buf, z);
+    //printf("%s\n", z);
 }
 
 } // namespace ir
