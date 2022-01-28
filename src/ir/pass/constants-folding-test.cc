2a176f389a5679a8a1e99674368b4fadd0b49183
emptyland <cjvoid@gmail.com>
2022-01-28 14:41:37 +0800

    Add miss ASSERT_STREQ.

--- src/ir/pass/constants-folding-test.cc
+++ src/ir/pass/constants-folding-test.cc
@@ -75,7 +75,62 @@ TEST_F(ConstantsFoldingPassTest, Sanity) {
     base::PrintingWriter printer(base::NewMemoryWritableFile(&buf), true/*ownership*/);
     modules["main:main"]->PrintTo(&printer);
     
-    printf("%s\n", buf.data());
+    constexpr static const char z[] = R"(module main @main:main {
+source-files:
+    [0] tests/24-ir-constants-folding/src/main/main.yalx
+
+globals:
+    %0 = I32Constant i32 <100>
+    %1 = I32Constant i32 <200>
+
+functions:
+    fun $init(): void {
+    boot:
+        %0 = Closure ref[fun ()->void] <fun yalx/lang:lang.$init>
+        CallRuntime void ref[fun ()->void] %0, string "yalx/lang:lang" <PkgInitOnce>
+        Ret void
+    } // main:main.$init
+
+    fun issue1(): i32 {
+    entry:
+        Ret void i32 300
+    } // main:main.issue1
+
+    fun issue2(): i32 {
+    entry:
+        Ret void i32 -100
+    } // main:main.issue2
+
+    fun issue3(): i32 {
+    entry:
+        Ret void i32 20000
+    } // main:main.issue3
+
+    fun issue4(): i32 {
+    entry:
+        %0 = SDiv i32 i32 100, i32 0
+        Ret void i32 %0
+    } // main:main.issue4
+
+    fun issue5(): u8 {
+    entry:
+        Ret void u8 0
+    } // main:main.issue5
+
+    fun issue6(): u8, u8 {
+    entry:
+        Ret void u8 0, u8 1
+    } // main:main.issue6
+
+    fun main(): void {
+    entry:
+        Ret void
+    } // main:main.main
+
+} // @main:main
+)";
+    //printf("%s\n", buf.data());
+    ASSERT_STREQ(z, buf.data());
 }
 
 } // namespace ir
